8fe796b256092951f166d6d0d8d5367b7fb193a0
Muhammadkorumtallee <shuaibkorumtallee00@gmail.com>
2022-04-03 18:20:44 +0100

    driver location and icons added

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -1,19 +1,3 @@
-"""finalproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from unicodedata import name
 from xml.dom.minidom import Document
 from django.contrib import admin
@@ -39,8 +23,9 @@ driver_urlpatterns = [
     path('salary/', d_views.salary, name="salary"),
 
     path('delivery/available/', d_views.deliveries_available_page, name="deliveries_available"),
-    path('api/available_deliveries/', d_apis.available_deliveries_api, name="available_deliveries"),
     path('delivery/delivering/', d_views.delivering_delivery_page, name="delivering_delivery"),
+
+    path('api/available_deliveries/', d_apis.available_deliveries_api, name="available_deliveries"),
     path('api/available_delivering/', d_apis.available_delivering_api, name="available_delivering"),
 ]
 
